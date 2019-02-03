ec5f97f6e654e596df80ce88f8352fd697295ec4
Jean Lopes <jean.llopes@outlook.com>
2019-02-03 16:28:41 -0200

    Add project files.

---
+++ CSharp_Stack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
